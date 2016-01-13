c8b4d346f1adc3c5278e601fe507f3500bee1e88
yangwm <jxfzywm@163.com>
2016-01-13 16:17:48 +0800

    logkafka: add _install/lib for centos6.7

---
+++ _install/lib/setup.sh
@@ -0,0 +1,18 @@
+
+~/logkafka/_install/lib
+sudo cp libpcre* /usr/local/lib/
+sudo cp libuv* /usr/local/lib/
+sudo cp librdkafka* /usr/local/lib/
+
+cd /usr/local/lib/
+sudo ln -s libpcre2-8.so.0.2.0 libpcre2-8.so
+sudo ln -s libpcre2-8.so.0.2.0 libpcre2-8.so.0
+sudo ln -s libpcre2-posix.so.0.0.0 libpcre2-posix.so
+sudo ln -s libpcre2-posix.so.0.0.0 libpcre2-posix.so.0
+sudo ln -s libuv.so.1.0.0 libuv.so
+sudo ln -s libuv.so.1.0.0 libuv.so.1
+sudo ln -s librdkafka.so.1 librdkafka.so
+sudo ln -s librdkafka++.so.1 librdkafka++.so
+
+sudo ldconfig
+
